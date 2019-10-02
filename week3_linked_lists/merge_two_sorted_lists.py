eac6de28d8222c424db959916d2396ff083f381b
jrumacias <juan@jrmacias.com>
2019-10-01 23:47:28 -0700

    completed merge_two_sorted_lists.py

--- week3_linked_lists/merge_two_sorted_lists.py
+++ week3_linked_lists/merge_two_sorted_lists.py
@@ -48,8 +48,8 @@ l1.next = ListNode(2)
 l1.next.next = ListNode(4)
 
 l2 = ListNode(1)
-l1.next = ListNode(3)
-l1.next.next = ListNode(4)
+l2.next = ListNode(3)
+l2.next.next = ListNode(4)
 
 def print_linked_list(head):
     current = head
